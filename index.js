196a9f206af54ba6242217910390b1b63f1cc144
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-05-26 23:34:04 +0530

    Complete righting the calculate stats fn.
    - This function will calculate ao5, ao12, etc for that particular
      session.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ var Stopwatch = require('timer-stopwatch');
 var keypress = require('keypress');
 var clc = require('cli-color');
 var charm = require('charm')();
-var jf  =  require('jsonfile')
-var util  =  require('util')
+var jf  =  require('jsonfile');
+var util  =  require('util');
 
 charm.pipe(process.stdout);
 
@@ -39,26 +39,64 @@ stopwatch.on('time', function(time){
 charm.position(1, start_solve).write("Solving: " + (time.ms / 1000).toFixed(2));
 });
 
-stopwatch.on('done', function(){
+calcStats = function(){
+
+	var sum = 0.0;
+
+	if(solves_today.length >= 5)
+	for(var i=0; i<5; i+=1){
+		sum += solves_today[solves_today.length - 1 - i];
+	}
+	ao5 = sum / 5;
+
+	sum = 0.0;
+	
+	if(solves_today.length >= 12)
+	for(var i=0; i<12; i+=1){
+		sum += solves_today[solves_today.length - 1 - i];
+	}
+	ao12 = sum / 12;
+
+	sum = 0.0;
+	
+	for(var i=0; i<solves_today.length; i+=1){
+		sum += solves_today[i];
+	}
+	ao_session = sum / solves_today.length;
+
+	console.log("Session Statistics: ");
+	console.log(ao5 + "; " + ao12 + "; " + ao_session);
+	console.log(solves_today);
+}
+
+/*
+  stopwatch.on('done', function(){
 	console.log("\n");
 	console.log("This solve was " + stopwatch.ms);
 	console.log("-----------------------------------");
 	console.log("\n");
-});
-
+	});
+*/
 
 var solving = false;
 var inspecting = false;
 
-var start_inspect = 3;
-var start_solve = 4;
+var start_inspect = 4;
+var start_solve = 5;
+
+var last_solve = -1;
+
+var right_row_num = 50;
+
+var num_solves = 0;
+var solves_today = new Array();
+var ao5 = 0.0, ao12 = 0.0, ao_session = 0.0;
 
 process.stdin.on('keypress', function (ch, key) {
 
-	if(key.name == 'r'){
-		inspecting = false;
-		solving = false;
-		console.log("Everything reset for the next solve!");
+	if(key.name == 's'){
+		console.log("Session statistics");
+		console.log("Session started at " + start_time);
 	}
 
 	if(!inspecting && !solving && key.name == 'space'){
@@ -86,14 +124,41 @@ process.stdin.on('keypress', function (ch, key) {
 			stopwatch.stop();
 			stopwatch.reset(0);
 
+			charm.position(1, start_inspect);
+			charm.erase("end");
+
 			charm.position(1, start_solve);
 			charm.erase("end");
+
 			charm.position(1, start_inspect);
-			console.log(clc.red("Bot: ") + "That solve was " + (solveTime / 1000.0).toFixed(2) + ' seconds');
+			this_solve = (solveTime / 1000.0).toFixed(2);
+			console.log(clc.red("Bot: ") + "That solve was " + this_solve + ' seconds');
+			solves_today.push(parseFloat(this_solve));
+
+			if(last_solve < 0)
+				console.log(clc.red("Bot: ") + "Great start! Keep the cube twisting!");
+			else{
+				num_solves += 1;
+				calcStats();
+
+				charm.position(right_row_num, start_inspect);
+				if(num_solves < 5)
+					console.log(clc.red("Previous solve: ") + clc.blue(last_solve));
+				else
+					console.log(clc.red("Today's AO5: ") + clc.blue(ao5));
+			}
+
 			console.log(clc.blue("You: Press space to start a solve!"));
 
 			start_solve += 2;
 			start_inspect += 2;
+
+			if(last_solve < 0){
+				start_solve += 1;
+				start_inspect += 1;
+			}
+
+			last_solve = this_solve;
 
 		}
 
@@ -111,13 +176,16 @@ charm.reset();
 console.log(clc.red("Bot: Hey! Let's start solving!"));
 console.log(clc.blue("You: Press space to initiate a solve."));
 
-var right_row_num = 50;
 charm.position(right_row_num, 1);
 console.log(clc.green("Keyboard shortcuts"));
 charm.position(right_row_num, 2);
 console.log(clc.red("Press space to initiate a solve."));
 charm.position(right_row_num, 3);
-console.log(clc.blue("Press the letter t to see your session statistics"));
+console.log(clc.blue("Press letter s to see your session statistics"));
+
+var start_time = new Date();
+start_time = start_time.getHours() + ":" + start_time.getMinutes();
+console.log(start_time);
 
 /*
 var file  =  './data.json';
