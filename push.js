db636ea266b3dca902fe5973ff5ed50304c73933
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-01 23:27:23 +0530

    Store the solve times to a file in the ~/.cube directory
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- push.js
+++ push.js
@@ -2,7 +2,14 @@ exports.pushGist = function(solvetime, scramble){
 	
 	solvestring = solvetime + ", " + scramble + "\n";
 
-	file = "data.csv";
+	var fileExists = require('file-exists');
+
+	if(!fileExists(process.env.HOME + '/.cube/times.csv')){
+		var mkdirp = require('mkdirp');
+		mkdirp(process.env.HOME + '/.cube');
+	}
+
+	file = process.env.HOME + "/.cube/times.csv";
 
 	var fs = require('fs');
 
