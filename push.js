eb2b3ac675919f4e25d3b545a65834c32377240f
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-04 17:48:38 +0530

    Use xdg-base dir while writing solve times

--- push.js
+++ push.js
@@ -3,16 +3,16 @@ exports.pushGist = function(solvetime, scramble){
 	solvestring = solvetime + ", " + scramble + "\n";
 
 	var fileExists = require('file-exists');
+	var xdg = require('xdg-basedir');
+	var fs = require('fs');
+	var filepath = xdg.data + "/cube/times.csv";
 
-	if(!fileExists(process.env.HOME + '/.cube/times.csv')){
+	if(!fileExists(xdg.data + '/cube/times.csv')){
 		var mkdirp = require('mkdirp');
-		mkdirp(process.env.HOME + '/.cube');
+		mkdirp(xdg.data + '/cube');
+		fs.writeFileSync(filepath, solvestring);
 	}
 
-	file = process.env.HOME + "/.cube/times.csv";
-
-	var fs = require('fs');
-
-	fs.appendFileSync(file, solvestring);
-
+	else
+		fs.appendFileSync(filepath, solvestring);
 }
