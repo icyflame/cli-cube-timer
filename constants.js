2bed02139630ac7c70b225ef9ab03185e81a9362
Jan-Ka <jan-ka@users.noreply.github.com>
2017-10-01 20:56:19 +0200

    add LOCAL_SAVE_DIR to constants and build other values from that

--- constants.js
+++ constants.js
@@ -1,3 +1,10 @@
 var xdg = require('xdg-basedir');
-exports.LOCAL_FILE_PATH = xdg.data + '/cube/times.csv';
-exports.PUSHED_FILE_PATH = xdg.data + '/cube/pushed.csv';
+var path = require('path');
+
+var localSaveDir = path.join(xdg.data, '/cube');
+var localFilePath = path.join(localSaveDir, 'times.csv');
+var pushedFilePath = path.join(localSaveDir, 'pushed.csv');
+
+exports.LOCAL_SAVE_DIR = localSaveDir;
+exports.LOCAL_FILE_PATH = localFilePath;
+exports.PUSHED_FILE_PATH = pushedFilePath;
