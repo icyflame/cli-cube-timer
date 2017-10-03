fd293648d12802725df55162d8674a5fa9c0e2bf
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-10-03 23:11:44 +0530

    reduce plus operations, export result directly

--- constants.js
+++ constants.js
@@ -1,9 +1,5 @@
 var xdg = require('xdg-basedir');
 
-var localSaveDir = xdg.data + '/cube';
-var localFilePath = localSaveDir + '/' + 'times.csv';
-var pushedFilePath = localSaveDir + '/' + 'pushed.csv';
-
-exports.LOCAL_SAVE_DIR = localSaveDir;
-exports.LOCAL_FILE_PATH = localFilePath;
-exports.PUSHED_FILE_PATH = pushedFilePath;
+var localSaveDir = exports.LOCAL_SAVE_DIR = xdg.data + '/cube';
+exports.LOCAL_FILE_PATH = localSaveDir + '/times.csv';
+exports.PUSHED_FILE_PATH = localSaveDir + '/pushed.csv';
