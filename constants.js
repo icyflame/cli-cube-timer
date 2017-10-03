52472356bfc239af713ab1e017ad3844be9c179b
Jan Ka <jan.kaltenecker@outlook.com>
2017-10-03 19:19:37 +0200

    removed path function calls from constants #41 review changes

--- constants.js
+++ constants.js
@@ -1,9 +1,8 @@
 var xdg = require('xdg-basedir');
-var path = require('path');
 
-var localSaveDir = path.join(xdg.data, '/cube');
-var localFilePath = path.join(localSaveDir, 'times.csv');
-var pushedFilePath = path.join(localSaveDir, 'pushed.csv');
+var localSaveDir = xdg.data + '/cube';
+var localFilePath = localSaveDir + '/' + 'times.csv';
+var pushedFilePath = localSaveDir + '/' + 'pushed.csv';
 
 exports.LOCAL_SAVE_DIR = localSaveDir;
 exports.LOCAL_FILE_PATH = localFilePath;
