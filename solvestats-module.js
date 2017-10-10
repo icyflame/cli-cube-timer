4e084a2f49520e5a6de0e1c700a2beb1085f8ca0
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-10-10 14:11:17 +0530

    remove a lot of unnecessary lines

--- solvestats-module.js
+++ solvestats-module.js
@@ -6,13 +6,7 @@ exports.calcStats = function (solves_today) {
   var min = require('lodash.min');
   var max = require('lodash.max');
 
-  var sum = 0.0;
-  var total_solves = solves_today.length;
-  var last_five = [];
-  var last_twelve = [];
-  var ao5;
-  var ao12;
-  var ao_session;
+  var ao_session, ao5, ao12, sum = 0.0, total_solves = solves_today.length;
 
   var best_time = parseFloat(min(solves_today)) * 1000;
   var worst_time = parseFloat(max(solves_today)) * 1000;
@@ -21,24 +15,20 @@ exports.calcStats = function (solves_today) {
     // it is assumed that solves_today is chronologically ordered, beginning with the oldest
     var descendingTotalSolves = solves_today.slice().reverse();
 
-    if(total_solves >= 5) {
-      last_five = descendingTotalSolves.slice(0, 5);
-    }
+    ao_session = mathAvg(solves_today);
 
-    ao5 = mathAvg(removeMinMax(last_five));
+    if(total_solves >= 5) {
+      ao5 = mathAvg(removeMinMax(descendingTotalSolves.slice(0, 5)));
+    }
 
     if(total_solves >= 12) {
-      last_twelve = descendingTotalSolves.slice(0, 12);
+      ao12 = mathAvg(removeMinMax(descendingTotalSolves.slice(0, 12)));
     }
-
-    ao12 = mathAvg(removeMinMax(last_twelve));
-
-    ao_session = mathAvg(solves_today);
   }
 
   ao5 = parseFloat(ao5) * 1000;
   ao12 = parseFloat(ao12) * 1000;
   ao_session = parseFloat(ao_session) * 1000;
 
-  return {ao5, ao12, ao_session, best_time, worst_time}
+  return { ao5, ao12, ao_session, best_time, worst_time }
 };
