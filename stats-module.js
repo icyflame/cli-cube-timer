bf828febe6110bacb609d12735ddfbbebb9a1507
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-12-19 11:24:09 +0530

    merge sovled and push files while printing stats
    Fix #44

--- stats-module.js
+++ stats-module.js
@@ -1,12 +1,10 @@
 module.exports = function (bucket_size) {
   bucket_size = bucket_size || 10;
 
+  var async = require('async');
   var fileModule = require('./file-module.js');
-  var file_name = require('./constants.js').PUSHED_FILE_PATH;
-
-  if (!fileModule.pushedFileExists()) {
-    file_name = fileModule.checkLocalFile();
-  }
+  var pushed_file_name = require('./constants.js').PUSHED_FILE_PATH;
+  var local_file_name = require('./constants.js').LOCAL_FILE_PATH;
 
   var fs = require('fs');
   var csv = require('fast-csv');
@@ -20,41 +18,60 @@ module.exports = function (bucket_size) {
     return prettyMs(ms, {verbose: true, secDecimalDigits: 2});
   }
 
-  var stream = fs.createReadStream(file_name);
   var all_times = new Stats({bucket_precision: bucket_size});
 
-  var csvStream = csv()
-    .on('data', function (data) {
-      if (data[0] !== 'DNF') {
-        // isolate the time
-        all_times.push(parseFloat(data[0]));
+  var avail_files = [ ];
+
+  if (fileModule.pushedFileExists()) {
+    avail_files.push(pushed_file_name);
+  }
+
+  if (fileModule.localFileExists()) {
+    avail_files.push(local_file_name);
+  }
+
+  async.eachSeries(avail_files,
+    function (file_name, callback) {
+      var csvStream = csv()
+      .on('data', function (data) {
+        if (data[0] !== 'DNF') {
+          all_times.push(parseFloat(data[0]));
+        }
+      })
+      .on('end', function () {
+        return callback();
+      });
+      var stream = fs.createReadStream(file_name);
+      stream.pipe(csvStream);
+    }, function (err) {
+    if (err) {
+      console.log("OOPS! There was an error reading the files");
+      console.error(err);
+    } else {
+      if (all_times.length <= 0) {
+        console.log(clc.red("No solves yet!"));
+        return;
       }
-    }).on('end', function () {
-    if (all_times.length <= 0) {
-      console.log(clc.red("No solves yet!"));
-      return;
+      console.log('\n');
+      console.log('Number of stored solves: ' + clc.green(all_times.length));
+      console.log('Mean of all solves: ' + clc.green(prettifyVerbose(all_times.amean() * 1000)));
+      console.log('Standard Deviation: ' + clc.green(prettifyVerbose(all_times.stddev() * 1000)));
+
+      var range = all_times.range();
+      console.log('Best Solve: ' + clc.green(prettifyVerbose(range[0] * 1000)));
+      console.log('Worst Solve: ' + clc.green(prettifyVerbose(range[1] * 1000)));
+      // console.log(all_times.distribution())
+
+      var distribution = all_times.distribution();
+
+      var input_obj = {};
+
+      distribution.forEach(function (e) {
+        input_obj[clc.blue(e.count + ' (' + e.range[0] + '+)')] = e.count;
+      });
+
+      barHorizontal(input_obj, { labels: true });
+
     }
-    console.log('\n');
-    console.log('Number of stored solves: ' + clc.green(all_times.length));
-    console.log('Mean of all solves: ' + clc.green(prettifyVerbose(all_times.amean() * 1000)));
-    console.log('Standard Deviation: ' + clc.green(prettifyVerbose(all_times.stddev() * 1000)));
-
-    var range = all_times.range();
-    console.log('Best Solve: ' + clc.green(prettifyVerbose(range[0] * 1000)));
-    console.log('Worst Solve: ' + clc.green(prettifyVerbose(range[1] * 1000)));
-    // console.log(all_times.distribution())
-
-    var distribution = all_times.distribution();
-
-    var input_obj = {};
-
-    distribution.forEach(function (e) {
-      input_obj[clc.blue(e.count + ' (' + e.range[0] + '+)')] = e.count;
-    });
-
-    barHorizontal(input_obj, { labels: true });
-
   });
-
-  stream.pipe(csvStream);
 };
