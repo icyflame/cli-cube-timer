38629fae69e776556d19306d85b00f875c570cd8
Siddharth Kannan <mail@siddharthkannan.in>
2019-11-16 22:59:13 +0900

    Fix the messages printed by the stats command

--- stats-module.js
+++ stats-module.js
@@ -55,26 +55,22 @@ module.exports = function ({ bucket, min, max }) {
         console.log(clc.red("No solves yet!"));
         return;
       }
-      console.log('\n');
-      console.log('Number of stored solves: ' + clc.green(all_times.length));
-      console.log('Mean of all solves: ' + clc.green(prettifyVerbose(all_times.amean() * 1000)));
-      console.log('Standard Deviation: ' + clc.green(prettifyVerbose(all_times.stddev() * 1000)));
+      console.log('Number of solves: ' + clc.green(all_times.length));
+      console.log('Mean: ' + clc.green(prettifyVerbose(all_times.amean() * 1000)));
+      console.log('Median: ' + clc.green(prettifyVerbose(all_times.median() * 1000)));
+      console.log('Standard deviation: ' + clc.green(prettifyVerbose(all_times.stddev() * 1000)));
 
       var range = all_times.range();
-      console.log('Best Solve: ' + clc.green(prettifyVerbose(range[0] * 1000)));
-      console.log('Worst Solve: ' + clc.green(prettifyVerbose(range[1] * 1000)));
-      // console.log(all_times.distribution())
+      console.log('Best solve: ' + clc.green(prettifyVerbose(range[0] * 1000)));
+      console.log('Worst solve: ' + clc.green(prettifyVerbose(range[1] * 1000)));
 
       var distribution = all_times.distribution();
-
       var input_obj = {};
-
       distribution.forEach(function (e) {
         input_obj[clc.blue(e.count + ' (' + e.range[0] + '+)')] = e.count;
       });
 
       barHorizontal(input_obj, { labels: true });
-
     }
   });
 };
